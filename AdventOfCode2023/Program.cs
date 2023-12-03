c1fee4fce665e2e473a2aa184a326cd20232eebc
rhysr <rhysr@DESKTOP-H0KMKQN>
2023-12-03 11:58:50 +0000

    Add project files.

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To run a solution, set one of the question projects as the start-up project.");
+        }
+    }
+}
